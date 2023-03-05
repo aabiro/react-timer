d0d1dc1cd219dc33df799a503c4c3ca6cf88969d
Biro, Aaryn <aaryn.biro@neurolign.com>
2023-03-05 05:34:02 -0500

    adding top level await

--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -6,15 +6,11 @@ const client = new MongoClient(connectionString);
 
 let conn;
 
-const connectToDb = async () => {
-  try {
-    conn = await client.connect();
-  } catch(e) {
-    console.error(e);
-  }
+try {
+  conn = await client.connect();
+} catch(e) {
+  console.error(e);
 }
-
-connectToDb();
 
 let db = conn.db("countdown_timer_flutter");
 
