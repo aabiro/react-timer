310c6bd326db2b97e4135216e9e2c324e4582351
Biro, Aaryn <aaryn.biro@neurolign.com>
2023-03-05 13:39:31 -0500

    adding top level await

--- server/index.mjs
+++ server/index.mjs
@@ -1,24 +1,34 @@
-import express from "express";
-import cors from "cors";
-import "./loadEnvironment.mjs";
-import "express-async-errors";
-import countdowns from "./routes/countdowns.mjs";
+// import express from "express";
+// import cors from "cors";
+// import "./loadEnvironment.mjs";
+// import "express-async-errors";
+// import countdowns from "./routes/countdowns.mjs";
 
-const PORT = process.env.PORT || 5050;
-const app = express();
+// const PORT = process.env.PORT || 5050;
+// const app = express();
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-// Load the /countdowns routes
-app.use("/countdowns", countdowns);
+// // Load the /countdowns routes
+// app.use("/countdowns", countdowns);
 
-// Global error handling
-app.use((err, _req, res, next) => {
-  res.status(500).send("Uh oh! An unexpected error occured.")
-})
+// // Global error handling
+// app.use((err, _req, res, next) => {
+//   res.status(500).send("Uh oh! An unexpected error occured.")
+// })
 
-// start the Express server
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+// // start the Express server
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port: ${PORT}`);
+// });
+
+var express = require("express");
+var app = express();
+
+
+app.get("/",function(req,res){
+        res.send("<h1>Oi mate</h1>");
 });
+
+app.listen(80);
